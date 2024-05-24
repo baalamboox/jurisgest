b7b2aed4e8bad8e88fbca904534b2bc6ce0d85bb
Jair <jairr8076@gmail.com>
2024-05-24 13:39:31 -0600

    Se diseño la pagina de coctacto

--- view/plantillas/secciones/contactanos.php
+++ view/plantillas/secciones/contactanos.php
@@ -1,66 +1,67 @@
 <div class="row">
     <div class="col">
-
-    
-<!-- 
-            <h3>Nos Ubicamos en:</h3>
-            
-            <p class="">Zacatecas 230, 4° piso, Col. Roma, a una calle de Insurgentes (Metrobus Sonora)</p>
-        
-        <div class="custom_heading-container   espacio_padding">
-            <h3 class=" ">
-                Telefonos
-            </h3>
-            
-            <p>55 78 62 11</p> 
-            <p>55 78 62 23</p>
-            <p>55 78 64 70</p>
-            <p>57 09 99 92</p>
-            <p>57 09 07 68</p>
-            
+        <h3 class="text-center text-gold-light">Nos Ubicamos en:</h3>
+        <br>
+        <div class="text-center">
+            <p >Zacatecas 230, 4° piso, Col. Roma, a una calle de Insurgentes (Metrobus Sonora)</p>
+            <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.7375691171718!2d-99.16548633047104!3d19.414554263713878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff392984bb55%3A0x809c450148c83099!2sZacatecas%20230-4%20%C2%B0%20piso%2C%20Roma%20Nte.%2C%20Cuauht%C3%A9moc%2C%2006700%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1716569928411!5m2!1ses!2smx" width="500" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-        <div class="custom_heading-container   espacio_padding2">
-            <h3 class=" ">
-                Correo Electrónico:
-            </h3>
+    </div>
+</div>
+<div class="row espacio_padding">    
+    <div class="col-md-6">
+        <div class="card">
+            <h5 class="card-header">Telefonos</h5>
+            <div class="card-body">
+                <p class="card-title text-left"><i  class="fas fa-phone-alt text-gold-light"></i> 55 78 62 11</p>
+                <p class="card-title text-right"><i class="fas fa-phone-alt text-gold-light"></i> 55 78 62 23</p>
+                <p class="card-title text-left"><i  class="fas fa-phone-alt text-gold-light"></i> 55 78 64 70</p>
+                <p class="card-title text-right"><i class="fas fa-phone-alt text-gold-light"></i> 57 09 99 92</p>
+                <p class="card-title text-left"><i  class="fas fa-phone-alt text-gold-light"></i> 57 09 07 68</p>
+            </div>
         </div>
-        <h5 class="text-center">lic.gerardocuellar@prodigy.net.mx</h5>
-        <div class="custom_heading-container  espacio_padding">
-            <h3 class=" ">
-                Envianos tus Comentarios:
-            </h3>
-            <p>Si lo prefieres por favor llena el siguiente formulario para contáctanos Gacias</p>
+    </div>
+    <div class="col-md-6">
+        <div class="card text-center">
+            <h5 class="card-header">Correo Electroonico</h5>
+            <div class="card-body text-center">
+                <h5 class="card-title"><i class="fas fa-envelope text-gold-light"></i><a class="text-gold-light" href="mailto:lic.gerardocuellar@prodigy.net.mx"> lic.gerardocuellar@prodigy.net.mx</a></h5>
+                <br>
+                <br>
+                <br>
+            </div>
         </div>
-        
-
-        <form>
-            <div class="form-group">
-                <label for="name">Nombre *</label>
-                <input type="text" class="form-control" id="name" placeholder="Ingresa tu nombre">
+    </div>
+</div>
+<div class="row espacio_paddimg">
+    <div class="col">
+        <div class="card">
+            <h5 class="card-header text-center">Envianos tu comentario:</h5>
+            <div class="card-body">
+                <p class="card-text">Si lo prefieres por favor llena el siguiente formulario para contáctanos Gracias!.</p>
+                <br>
+                <form>
+                    <div class="row">
+                        <div class="col form-group text-center">
+                            <label for="">Nombre</label>
+                            <input type="text" class="form-control" placeholder="Nombre">
+                        </div>
+                        <div class="col form-group text-center">
+                            <label for="">Correo</label>
+                            <input type="email" class="form-control" placeholder="Correo">
+                        </div>
+                        <div class="col form-group text-center">
+                            <label for="">Tema</label>
+                            <input type="text" class="form-control" placeholder="Tema">
+                        </div>
+                    </div>
+                    <div class="form-group text-center">
+                        <label for="exampleFormControlTextarea1">Descripcion</label>
+                        <textarea class="form-control gold-border-input" id="exampleFormControlTextarea1"  placeholder="Descripcion" rows="3"></textarea>
+                    </div>
+                </form>
+                <a href="#" class="btn btn-gold-light btn-block btn-sm mt-4">Enviar</a>
             </div>
-
-            <div class="form-group">
-                <label for="email">Correo Electrónico *</label>
-                <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo electrónico">
-            </div>
-
-            <div class="form-group">
-                <label for="subject">Tema *</label>
-                <input type="text" class="form-control" id="subject" placeholder="Ingresa el tema de tu consulta">
-            </div>
-
-            <div class="form-group">
-                <label for="message">Descripción *</label>
-                <textarea class="form-control" id="message" rows="3"></textarea>
-            </div>
-
-            <div class="form-group">
-                <label for="verification">Código de Verificación</label>
-                <input type="text" class="form-control" id="verification" placeholder="Ingresa el código de verificación">
-                <small id="verificationHelp" class="form-text text-muted">Si no tienes un código, presiona <a href="#">aquí</a> para obtener uno.</small>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Enviar</button>
-        </form> -->
+        </div>
     </div>
 </div>
