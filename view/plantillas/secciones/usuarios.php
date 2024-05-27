c933b21ca3d4b735095d67ce12e374ae3a7ab17f
baalamboox <jmg2010982@gmail.com>
2024-05-27 17:36:53 -0600

    Comenzando la sección de usuarios

---
+++ view/plantillas/secciones/usuarios.php
@@ -0,0 +1,29 @@
+<div class="row">
+    <div class="col-md-8"></div>
+    <div class="col-md-4">
+        <div class="card border-0">
+            <div class="card-header bg-gray-special text-gold-light">
+                <i class="fas fa-list-ul mr-2"></i>
+                Operaciones
+            </div>
+            <div class="card-body">
+                <div class="card border-0">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <span class="btn btn-sm btn-gold-light btn-block">Crear usuario</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="btn btn-sm btn-gold-light btn-block">Crear usuario</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="btn btn-sm btn-gold-light btn-block">Crear usuario</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="btn btn-sm btn-gold-light btn-block">Crear usuario</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
