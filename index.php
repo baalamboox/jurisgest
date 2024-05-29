76cd61d90b2181699932c90609b1e5cc3adc5893
baalamboox <jmg2010982@gmail.com>
2024-05-28 21:59:18 -0600

    Index, ob_start agregado

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
+    ob_start();
 ?>
 <!DOCTYPE html>
 <html lang="es-MX" dir="ltr">
@@ -60,7 +61,6 @@
                         break;
                 }
             } else {
-                ob_start();
                 header("location:inicio-sesion");
                 exit();
                 ob_end_flush();
