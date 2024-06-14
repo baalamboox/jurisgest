a80fc3ff91901ef6a4c2e74ffcb6adcea6814b02
baalamboox <jmg201098@gmail.com>
2024-06-14 11:01:20 -0600

    Compresión de imagenes de fondo

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ window.onload = function() {
     // Selección y comparació de vistas para el manejo aleatorio de fondos para el inicio de sesión.
     const body = document.querySelector("body");
     if(window.location.pathname == "/" || window.location.pathname == "/inicio-sesion") {
-        body.style.backgroundImage = `url(public/images/fondos/fondo-${Math.floor(Math.random() * 3)}.jpeg)`;
+        body.style.backgroundImage = `url(public/images/fondos/fondo-${Math.floor(Math.random() * 3)}.webp)`;
         body.style.backgroundPosition = "center";
         body.style.backgroundSize = "cover";
         body.style.backgroundRepeat = "no-repeat";
